5dc48a5dd782c90035b73ee68de4328c08894191
Brandon Lyons <lyonsbp@umich.edu>
2020-11-17 18:15:15 -0500

    Add typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ListingsController } from './listings/listings.controller';
 import { ListingsService } from './listings/listings.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'local',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, ListingsController],
   providers: [AppService, ListingsService],
 })
